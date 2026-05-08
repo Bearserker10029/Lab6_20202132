2a3d6b9b300c5b8de4ec7efed5f75bfd29276591
Sergio Meneses Ramos <a20223291@pucp.edu.pe>
2026-05-07 19:40:58 -0500

    Initial commit

---
+++ src/main/java/org/example/lab5_20202132/Lab520202132Application.java
@@ -0,0 +1,13 @@
+package org.example.lab5_20202132;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520202132Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab520202132Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab5_20202132/Lab520202132ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab5_20202132;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520202132ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
